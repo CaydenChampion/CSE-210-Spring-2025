596c63d8ddcb41b41386d4ee2c7581755ba95ded
Cayden <cha24075@byui.edu>
2025-04-30 10:08:38 -0600

    changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,28 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    // {
+    //     Console.Write("Input your first name: ");
+    //     // Console.Write if you want to input your name on the same line in the terminal.
+    //     string firstname = Console.ReadLine();
+    //     Console.WriteLine ($"What is your first name? {firstname}");
+
+    //     Console.Write("Input your last name: ");
+    //     string lastname = Console.ReadLine();
+    //     Console.WriteLine ($"What is your last name? {lastname}");
+    //     Console.WriteLine($"Your name is: {lastname}, {firstname} {lastname}.");
+    // }
+    
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.Write("Input your first name: ");
+        // Console.Write if you want to input your name on the same line in the terminal.
+        Console.Write ("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        // Console.Write("Input your last name: ");
+        Console.Write ("What is your last name? ");
+        string lastname = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is: {lastname}, {firstname} {lastname}.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Input your first name: ");
+        // // Console.Write if you want to input your name on the same line in the terminal.
+        // string firstname = Console.ReadLine();
+        // Console.WriteLine($"Your first name is: {firstname}");
+        // string lastname = Console.ReadLine();
+        // Console.WriteLine($"Your last name is: {lastname}, {firstname} {lastname}.");
+
+        // int x = 10;
+        // if (x==10)
+        // {
+        //     Console.WriteLine ("X is 10");
+        // }
+        Console.WriteLine("How are you?");
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"Hello Bob {i}");
+        }
     }
 }
