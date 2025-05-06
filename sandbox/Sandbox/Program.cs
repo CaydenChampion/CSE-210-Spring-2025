7ae87a10111cb373233a444737a9889df94324d5
Cayden <cha24075@byui.edu>
2025-05-06 11:14:08 -0600

    in class changes 5/6/25

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Diagnostics.Tracing;
+using System.IO.Compression;
+
+class Circle
+{
+    private double _radius;
+
+    public void SetRadius(double radius)
+    {
+        if (radius < 0)
+        {
+            Console.WriteLine ("Error, radius must > 0");
+            return;
+        }
+        _radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine($"{myCircle.GetRadius()}");
+        Circle myCircle2 = new Circle();
+        myCircle2.SetRadius(20);
+        Console.WriteLine($"{myCircle2.GetRadius()}");
+
+        Console.WriteLine($"{myCircle.GetArea()}");
+        Console.WriteLine($"{myCircle2.GetArea()}");
+    }
+}
+
+
+
+
+// class Program
+// {
+    // static double AddNumbers (double a, double b)
+    // {
+    //     return a + b;
+    // }
+
+    // static void DisplayGreeting()
+    // {
+    //     Console.WriteLine("Hey Bob, How are you?");
+    // }
+
+    // static void Main(string[] args)
+    // {
         // Console.WriteLine("Input your first name: ");
         // // Console.Write if you want to input your name on the same line in the terminal.
         // string firstname = Console.ReadLine();
@@ -16,12 +73,42 @@ class Program
         // {
         //     Console.WriteLine ("X is 10");
         // }
-        Console.WriteLine("How are you?");
-        for (int i = 0; i < 20; i++)
-        {
-            Console.WriteLine($"Hello Bob {i}");
-            // hi
-            //hi 2
-        }
-    }
-}
+        // Console.WriteLine("How are you?");
+        // for (int i = 0; i < 20; i++)
+        // {
+        //     Console.WriteLine($"Hello Bob {i}");
+        // }
+        // int x = 0;
+        // int y = x++;
+        // Console.WriteLine(x);
+        // Console.WriteLine(y);
+        // y = ++x;
+
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(99);
+        // myNumbers.Add(25);
+        // myNumbers.Add(-132);
+
+        // foreach(int i in myNumbers)
+        // {
+        //     Console.WriteLine(i);
+        // }
+        // double total = AddNumbers (123.98, 985.12356)
+        // Console.WriteLine(total);
+        
+        // DisplayGreeting();
+        // DisplayGreeting();
+
+        // bool done = false;
+        // while (!done)
+        // {
+        //     done = true;
+        // }
+        // do
+        // {
+
+        // } while (!done);
+
+
+    // }
+// }
