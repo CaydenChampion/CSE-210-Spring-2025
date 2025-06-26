33f85dafc59264f8dbb55e0a540e4f4ea9529e02
Cayden <cha24075@byui.edu>
2025-06-26 11:03:24 -0600

    Sandbox inheritance update

--- Person/PoliceMan.cs
+++ Person/PoliceMan.cs
@@ -1,15 +1,34 @@
 class PoliceMan : Person
 {
     private string _weapons;
-    public PoliceMan(string firstName, string lastName, int age, string weapons)
+    private int _hoursWorked;
+    private double _hourlyWage;
+    public PoliceMan(string firstName, string lastName,
+    int age, string weapons, int hoursWorked, double hourlyWage)
     : base(firstName, lastName, age)
     {
         _weapons = weapons;
+        _hourlyWage = hourlyWage;
+        _hoursWorked = hoursWorked;
     }
 
     public string GetPoliceManInformation()
     {
-        return $"Weapons: {_weapons} :: {GetPersonInformation()}";
+        return $"Weapons: {_weapons} :: {base.GetPersonInformation()}";
     }
+    public override string GetPersonInformation()
+    {
+        return $"I am a Police Man: Tools: {_weapons} :: {base.GetPersonInformation()}";
+    }
+    public override double GetPay()
+    {
+        double pay = _hourlyWage * _hoursWorked;
+        if (_hoursWorked > 40)
+        {
+            // pay -= (_hoursWorked - 40) * _hourlyWage;
+            pay += (_hoursWorked - 40) * _hourlyWage * 2;
+        }
+        return pay;
+    } 
 }
 
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,46 +1,79 @@
 
-// using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine(" ");
-//     }
-// }
+// Spinner
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
+using System.IO.Compression;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine(" ");
+
+        int duration = 9;
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = currentTime.AddSeconds(duration);
+        int sleepTime = 250;
+        int count = duration;
+
+        string animationString = "(^_^)(-_-)";
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(animationString[0..5]);
+            Console.Write("\b\b\b\b\b");
+            Console.Write(animationString[5..]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b\b\b\b\b");
+        }
+
+        while (DateTime.Now < endTime)
+            {
+                Console.Write("+");
+                Thread.Sleep(sleepTime);
+                Console.Write("\b");
+                Console.Write("-");
+                Thread.Sleep(sleepTime);
+                Console.Write("\b");
+            }
+    }
+}
 
 
 
 
 //Circle Program//
 
-using System;
-using System.Diagnostics.Tracing;
-using System.IO.Compression;
+// using System;
+// using System.Diagnostics.Tracing;
+// using System.IO.Compression;
 
 
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Circle myCircle = new Circle();
-        myCircle.SetRadius(10);
-        Console.WriteLine($"{myCircle.GetRadius()}");
-        Circle myCircle2 = new Circle();
-        myCircle2.SetRadius(20);
-        Console.WriteLine($"{myCircle2.GetRadius()}");
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Circle myCircle = new Circle();
+//         myCircle.SetRadius(10);
+//         Console.WriteLine($"{myCircle.GetRadius()}");
+//         Circle myCircle2 = new Circle();
+//         myCircle2.SetRadius(20);
+//         Console.WriteLine($"{myCircle2.GetRadius()}");
 
-        Console.WriteLine($"{myCircle.GetArea()}");
-        Console.WriteLine($"{myCircle2.GetArea()}");
+//         Console.WriteLine($"{myCircle.GetArea()}");
+//         Console.WriteLine($"{myCircle2.GetArea()}");
 
-        Cylinder myCylinder = new Cylinder();
-        myCylinder.SetHeight(10);
-        myCylinder.SetCircle(myCircle);
-        Console.WriteLine($"{myCylinder.GetVolume()}");
+//         Cylinder myCylinder = new Cylinder();
+//         myCylinder.SetHeight(10);
+//         myCylinder.SetCircle(myCircle);
+//         Console.WriteLine($"{myCylinder.GetVolume()}");
         
-    }
-}
+//     }
+// }
 
 // End of Circle program //
 
